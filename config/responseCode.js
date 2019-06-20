d685f94bb778a768e619b5a52cff2a5907927742
tangpeng <tangpeng@10cifang.com>
2019-06-20 14:36:27 +0800

    111

---
+++ config/responseCode.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // 正常
+  normalCode: 1000,
+  // 参数异常
+  paramsErrorCode: 1001,
+  // 系统异常
+  systemErrorCode: 1002,
+  // 用户未登录
+  loginErrorCode: 1003,
+  // 请求失败
+  requestErrorCode: 1004,
+  // 权限不足
+  permissionErrorCode: 1005,
+  // 用户已登录
+  loginOutErrorCode: 1006,
+
+}
