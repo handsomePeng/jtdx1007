971ae28b37027dcb4d007e345ed1e4b4d6422706
tangpeng <tangpeng@10cifang.com>
2019-07-29 11:24:40 +0800

    录入时间bug修改

--- routes/user.js
+++ routes/user.js
@@ -20,11 +20,11 @@ router.use((req, res, next) => {
 
 const date = new Date(),
   yy = date.getFullYear(),
-  MM = date.getMonth() + 1,
-  dd = date.getDate(),
-  hh = date.getHours(),
-  mm = date.getMinutes(),
-  ss = date.getSeconds()
+  MM = (date.getMonth() + 1) >= 10 ? (date.getMonth() + 1) : ('0'+(date.getMonth() + 1)),
+  dd = (date.getDate()) >= 10 ? (date.getDate()) : ('0'+(date.getDate())),
+  hh = (date.getHours()) >= 10 ? (date.getHours()) : ('0'+(date.getHours())),
+  mm = (date.getMinutes()) >= 10 ? (date.getMinutes()) : ('0'+(date.getMinutes())),
+  ss = (date.getSeconds()) >= 10 ? (date.getSeconds()) : ('0'+(date.getSeconds()))
 
 const  time = yy + '-' + MM + '-' + dd + ' ' + hh + ':' + mm + ':' + ss
 
